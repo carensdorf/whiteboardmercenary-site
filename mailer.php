9439a7cdca6a2f6638842a2139ec5ecfc591db0a
Chris Arensdorf <carensdorf@cloudscionconsulting.com>
2025-07-21 18:00:56 -0400

    Update CTA to request demo; added demo page

--- mailer.php
+++ mailer.php
@@ -2,15 +2,17 @@
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-		$name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $first_name = strip_tags(trim($_POST["first_name"]));
+        $first_name = str_replace(array("\r","\n"),array(" "," "),$first_name);
+        $last_name = strip_tags(trim($_POST["last_name"]));
+        $last_name = str_replace(array("\r","\n"),array(" "," "),$last_name);
+        $company = strip_tags(trim($_POST["company"]));
+        $company = str_replace(array("\r","\n"),array(" "," "),$company);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL); 
-        $message = trim($_POST["message"]);
         $phone = trim($_POST["phone"]);
-        $website = trim($_POST["website"]);
 
         // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if ( empty($first_name) || empty($last_name) || empty($company) || empty($phone) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
             echo "Oops! There was a problem with your submission. Please complete the form and try again.";
@@ -18,31 +20,30 @@
         }
 
         // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "support@rstheme.com";
+        $recipient = "carensdorf@cloudscionconsulting.com";
 
         // Set the email subject.
-        $subject = "New contact from $name";
+        $subject = "Demo Request from $first_name $last_name";
 
         // Build the email content.
-        $email_content = "Name: $name\n";
+        $email_content = "First Name: $first_name\n";
+        $email_content .= "Last Name: $last_name\n";
+        $email_content .= "Company: $company\n";
         $email_content .= "Email: $email\n";
         $email_content .= "Phone: $phone\n";
-        $email_content .= "Website: $website\n";
-        $email_content .= "Message:\n$message\n";
 
         // Build the email headers.
-        $email_headers = "From: $name <$email>";
+        $email_headers = "From: $first_name $last_name <$email>";
 
         // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Thank You! Your request has been sent.";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            echo "Oops! Something went wrong and we couldn't send your request.";
         }
 
     } else {
